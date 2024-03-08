d4cce9167b602e9f08f053fce21d6413fa8785df
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2024-03-08 09:49:58 -0500

    Inyectando el repositorio en el caso de uso

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,3 +1,6 @@
+import { LogEnitity, LogSeveritylevel } from "../../entities/log.entity";
+import { LogRepository } from "../../repository/log.repository";
+
 interface CheckServiceUseCase {
   execute(url: string): Promise<boolean>;
 }
@@ -7,6 +10,7 @@ type ErrorCallback = (error: string) => void;
 
 export class CheckService implements CheckServiceUseCase {
   constructor(
+    private readonly logRepository: LogRepository,
     private readonly successCallback: SuccessCallback,
     private readonly errorCallback: ErrorCallback
   ) {}
@@ -17,9 +21,19 @@ export class CheckService implements CheckServiceUseCase {
       if (!req.ok) {
         throw new Error(`Error on check service ${url}`);
       }
+      const log = new LogEnitity(
+        LogSeveritylevel.low,
+        `Service ${url} is working fine`
+      );
+      await this.logRepository.saveLog(log);
       this.successCallback();
       return true;
     } catch (error) {
+      const log = new LogEnitity(
+        LogSeveritylevel.high,
+        `Service ${url} is not working`
+      );
+      await this.logRepository.saveLog(log);
       this.errorCallback(`${error}`);
       return false;
     }
